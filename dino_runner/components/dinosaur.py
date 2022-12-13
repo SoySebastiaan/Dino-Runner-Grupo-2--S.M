080ef0fd65a625220611bfc228a2806dc4e93a0f
SoySebastiaan <sebastian.miranda@fundacion-jala.org>
2022-12-13 10:46:54 -0500

    Tarea realizada - Metodo duck
    Realizé el metodo duck con base a lo visto en clase.
    
    pdta: hay un pequeño bug

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,10 +1,11 @@
 import pygame
 from pygame.sprite import Sprite
-from dino_runner.utils.constants import RUNNING, JUMPING
+from dino_runner.utils.constants import DUCKING, RUNNING, JUMPING
 
 
 JUMPING_ACTION = "jumping"
 RUNNING_ACTION = "running"
+DUCKING_ACTION = "ducking"
 class Dinosaur(Sprite):
     Y_POS = 310
     X_POS = 80
@@ -24,6 +25,8 @@ class Dinosaur(Sprite):
             self.run()
         elif self.action == JUMPING_ACTION:
             self.jump()
+        elif self.action == DUCKING_ACTION:
+            self.duck()
 
         if self.action != JUMPING_ACTION:
             if user_input[pygame.K_UP]: 
@@ -31,6 +34,11 @@ class Dinosaur(Sprite):
             else:
                 self.action = RUNNING_ACTION
 
+        if self.action != DUCKING_ACTION:
+            if user_input[pygame.K_DOWN]:
+                self.action = DUCKING_ACTION
+            
+                
         if self.step >= 9:
             self.step = 0
 
@@ -47,6 +55,12 @@ class Dinosaur(Sprite):
             self.jump_velocity = self.JUMP_VELOCITY
             self.action = RUNNING_ACTION    
 
+    def duck(self):
+        self.image = DUCKING[0] if self.step < 5 else DUCKING[1]
+        self.rect = self.image.get_rect()
+        self.rect.x = 80
+        self.rect.y = 332
+        self.step += 1
 
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
