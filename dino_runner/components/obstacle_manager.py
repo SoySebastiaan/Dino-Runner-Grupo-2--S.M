12c4e33135c273120e83ea6821052744b112e3c6
SoySebastiaan <sebastian.miranda@fundacion-jala.org>
2022-12-14 11:34:51 -0500

    Tarea: añadir "LARGE_CACTUS" al juego.
    La realizé con el metodo random.randint, y le añadí valores enteros al cactus para poder utilizar el metodo randint.

--- dino_runner/components/obstacle_manager.py
+++ dino_runner/components/obstacle_manager.py
@@ -1,7 +1,8 @@
 import pygame
+import random
 from dino_runner.components.obstacles.cactus import Cactus
 from dino_runner.components.obstacles.obstacle import Obstacle
-from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS, SMALL_CACTUS
 
 
 class ObstacleManager:
@@ -10,9 +11,16 @@ class ObstacleManager:
         self.obstacles = []
 
     def update(self, game):
+        self.SMALL_CACTUS = 1
+        self.LARGE_CACTUS = 2
+        self.cactus = random.randint(self.SMALL_CACTUS, self.LARGE_CACTUS)
         if len(self.obstacles) == 0:
-            self.obstacles.append(Cactus(SMALL_CACTUS))
-        
+            if self.cactus % 2 == 0:
+                self.obstacles.append(Cactus(LARGE_CACTUS))
+            else:
+                self.obstacles.append(Cactus(SMALL_CACTUS))
+            
+
         for obstacle in self.obstacles:
             obstacle.update(game.game_speed, self.obstacles)
             if obstacle.rect.colliderect(game.player.rect):
